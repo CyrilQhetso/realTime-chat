7cd443d536ff8f1ca619301528b99ce56ceaf05b
CyrilQhetso <cyril.qhetso@gmail.com>
2025-05-28 14:52:48 +0200

    services

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,68 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../models/user';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { AuthRequest, AuthResponse, RegisterRequest } from '../models/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private currentUserSubject = new BehaviorSubject<User | null>(null);
+  public currentUser$ = this.currentUserSubject.asObservable();
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+    // Check if user is already looged in
+    const token = localStorage.getItem('token');
+    const user = localStorage.getItem('currentUser');
+    if (token && user) {
+      this.currentUserSubject.next(JSON.parse(user));
+    }
+   }
+
+   login(credentials: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/login`, credentials).pipe(
+      tap(response => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('currentUser', JSON.stringify(response.user));
+        this.currentUserSubject.next(response.user);
+      })
+    );
+   }
+
+   register(registerData: RegisterRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/auth/register`, registerData).pipe(
+      tap(response => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('currentUser', JSON.stringify(response.user));
+        this.currentUserSubject.next(response.user);
+      })
+    );
+   }
+
+  logout(): Observable<void> {
+    const username = this.currentUserSubject.value?.username;
+    return this.http.post<void>(`${this.apiUrl}/auth/logout`, username).pipe(
+      tap(() => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+      })
+    );
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getCurrentUser(): User | null {
+    return this.currentUserSubject.value;
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,9 +1,95 @@
 import { Injectable } from '@angular/core';
+import SockJS from 'sockjs-client';
+import { Client, IFrame, IMessage, Stomp } from '@stomp/stompjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Message } from '../models/message';
+import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
 
-  constructor() { }
+  private client: Client | null = null;
+  private stompConnected = new BehaviorSubject<boolean>(false);
+  private messageSubject = new Subject<Message>();
+
+  constructor(private authService: AuthService) { }
+
+  connect(): Observable<boolean> {
+    if (!this.client) {
+      const socket = new SockJS(`${environment.apiUrl}/ws`);
+      this.client = Stomp.over(socket);
+
+      this.client.connectHeaders = {
+        Authorization: `Bearer ${this.authService.getToken()}`
+      };
+
+      this.client.onConnect = (frame: IFrame) => {
+        console.log('Connected to WebSocket');
+        this.stompConnected.next(true);
+        this.subscribeToPrivateMessages();
+      };
+
+      this.client.onStompError = (frame: IFrame) => {
+        console.error('STOMP error', frame);
+        this.stompConnected.next(false);
+      };
+
+      this.client.activate();
+    }
+
+    return this.stompConnected.asObservable();
+  }
+
+  disconnect(): void {
+    if (this.client) {
+      this.client.deactivate();
+      this.client = null;
+      this.stompConnected.next(false);
+    }
+  }
+
+  subsribeToConversation(conversationId: string): void {
+    if (this.client && this.stompConnected.value) {
+      this.client.subscribe(`/topic/conversation/${conversationId}`, (message: IMessage) => {
+        const receivedMessage: Message = JSON.parse(message.body);
+        this.messageSubject.next(receivedMessage);
+      });
+    }
+  }
+
+  private subscribeToPrivateMessages():void {
+    if (this.client && this.stompConnected.value && this.authService.getCurrentUser()) {
+      const username = this.authService.getCurrentUser()!.username;
+      this.client.subscribe(`/user/${username}/queue/messages`, (message: IMessage) => {
+        const receivedMessage: Message = JSON.parse(message.body);
+        this.messageSubject.next(receivedMessage);
+      });
+    }
+  }
+
+  sendMessage(conversationId: string, content: string): void {
+    if (this.client && this.stompConnected.value) {
+      const currentUser = this.authService.getCurrentUser();
+      if (currentUser) {
+        this.client.publish({
+          destination: `/app/chat/${conversationId}`,
+          body: JSON.stringify({
+            senderUsername: currentUser.username,
+            content: content
+          })
+        });
+      }
+    }
+  }
+
+  getMessages(): Observable<Message> {
+    return this.messageSubject.asObservable();
+  }
+
+  isConnected(): boolean {
+    return this.stompConnected.value;
+  }
 }
