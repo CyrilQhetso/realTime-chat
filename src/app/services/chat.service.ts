d6dc7a3772b0cafa13dcd668f75cc3edf40e16fb
CyrilQhetso <cyril.qhetso@gmail.com>
2025-05-28 15:10:05 +0200

    chat service update

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { Conversation } from '../models/conversation';
+import { Message } from '../models/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor() { }
+  private apiUrl =  environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getAllUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getCurrentUser(): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/me`);
+  }
+
+  getUserConversations(): Observable<Conversation[]> {
+    return this.http.get<Conversation[]>(`${this.apiUrl}/conversations`);
+  }
+
+  getConversationById(id: string): Observable<Conversation> {
+    return this.http.get<Conversation>(`${this.apiUrl}/conversations/${id}`);
+  }
+
+  getConversationMessages(conversationId: string): Observable<Message[]> {
+    return this.http.get<Message[]>(`${this.apiUrl}/conversations/${conversationId}/messages`);
+  }
+
+  createOrGetPrivateConversation(otherUsername: string): Observable<Conversation> {
+    return this.http.post<Conversation>(`${this.apiUrl}/conversations/private`, { otherUsername });
+  }
+
+  createGroupConversation(name: string, participantUsernames: string[]): Observable<Conversation> {
+    return this.http.post<Conversation>(`${this.apiUrl}/conversations/group`, { name, participantUsernames });
+  }
+
+  markConversationAsRead(conversationId: string): Observable<void> {
+    return this.http.post<void>(`${this.apiUrl}/conversations/${conversationId}/read`, {});
+  }
 }
